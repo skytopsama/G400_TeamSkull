b4ce9693b95c31e1831924fea8aaa8eb8776c9c5
Aaron Griffin <aasgriff@indiana.edu>
2017-09-17 21:01:18 -0400

    Made test object to experiment with object snapping. MoveObject script allows for items to be moved with mouse selection. Items move and snap in grid-like fashion. Movement is still not smooth.

---
+++ G400_TeamSkull/Assets/Scripts/MoveObject.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveObject : MonoBehaviour {
+	bool isPicked = false;
+	int gridSize;
+	GameManager gm;
+
+	// Use this for initialization
+	void Start () {
+		gridSize  = GameObject.Find("Game Manager").GetComponent<GameManager>().gridSize;
+		
+	}
+	
+	// Update is called once per frame
+	void Update(){
+
+		if(Input.GetMouseButtonUp(0)){
+			isPicked = false;
+		}
+
+		if(isPicked == true){
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			transform.position = mousePos;
+
+			Vector3 currentPos = transform.position;
+			gameObject.transform.position = new Vector3(
+				Mathf.Round(currentPos.x), 
+				Mathf.Round(currentPos.y), 
+				Mathf.Round(currentPos.z));
+			
+			// if you want to smooth movement then lerp it
+
+		}
+
+	}
+
+	void OnMouseDown(){
+		isPicked = true;
+	}
+}
