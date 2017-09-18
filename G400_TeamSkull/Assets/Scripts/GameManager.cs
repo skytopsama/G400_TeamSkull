8d5c10e0a39b6f3b985693690b92fdaab4dd6eea
Aaron Griffin <aasgriff@indiana.edu>
2017-09-17 20:10:40 -0400

    Game Manager generates tiles on a n x n grid. Tiles are added to a grid game object

--- G400_TeamSkull/Assets/Scripts/GameManager.cs
+++ G400_TeamSkull/Assets/Scripts/GameManager.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
+	public int gridSize;
+	public GameObject groundTile;
 
 	// Use this for initialization
 	void Start () {
-		
+		Transform tileTransform = groundTile.GetComponentsInChildren<Transform>(true)[0];
+		Quaternion tileRotation = tileTransform.rotation;
+		for (int i = 0; i < gridSize; i++) {
+			for (int j = 0; j < gridSize; j++) {
+				var newTile = Instantiate (groundTile, new Vector3 (i, j, 0), tileRotation);
+				newTile.transform.parent = GameObject.Find ("Map Grid").transform;
+			}
+		}
+
 	}
 	
 	// Update is called once per frame
