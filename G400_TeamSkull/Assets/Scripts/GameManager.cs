b4a94873bd705798b5edabe52a27cb58ea259a4f
Aaron Griffin <aasgriff@indiana.edu>
2017-09-22 18:38:14 -0400

    Imported TDTK, created new scene

--- G400_TeamSkull/Assets/Scripts/GameManager.cs
+++ G400_TeamSkull/Assets/Scripts/GameManager.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class GameManager : MonoBehaviour {
 	public int gridSize;
 	public GameObject groundTile;
+	public GameObject playerTile;
 
 	// Use this for initialization
 	void Start () {
+		//Builds a grid
 		Transform tileTransform = groundTile.GetComponentsInChildren<Transform>(true)[0];
 		Quaternion tileRotation = tileTransform.rotation;
 		for (int i = 0; i < gridSize; i++) {
 			for (int j = 0; j < gridSize; j++) {
-				var newTile = Instantiate (groundTile, new Vector3 (i, j, 0), tileRotation);
+				var newTile = Instantiate (groundTile, new Vector3 (i, 0, j), tileRotation);
 				newTile.transform.parent = GameObject.Find ("Map Grid").transform;
 			}
 		}
+
+		GameObject.Find ("Map Grid").transform.position = new Vector3 (-(gridSize / 2), 0, -(gridSize / 2));
 
 	}
 	
